5e356ec7f0aba165e3989bbcf51d7766992bc497
thiguet <thiguet2014@gmail.com>
2016-03-18 14:06:53 -0300

    Agr funciona

--- uva.cpp
+++ uva.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 #include <cstdio>
 #include <deque>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
 	deque<int> deck;
+	string txt = "";
 	int ent;
-	cin >> ent;	
+	cin >> ent;
 	while(ent > 0) {
-		printf("%s", "Discarded cards: ");		
-		scanf("%d", ent);	
-		for(int i = 0 ; i < ent ; i++) {		
+		txt += "Discarded cards: ";
+		for(int i = 0 ; i < ent ; i++) {
 			deck.push_front(i + 1);
 		}
-		for(int i = ent ; i > 1 ; i--) {
-			printf("%s", deck.back() + ", ");
-			deck.pop_back();
-
-			deck.push_front(deck.back());
-
-			printf("%s", deck.back() + ", ");
-			deck.pop_back();
-		}	
-
 		
-		printf("%s", "\nRemaining card: ");	
-		printf("%s", + deck.back() + "\n" );
+		for(int i = ent ; i > 1 ; i--) {
+			txt += (deck.back() + 48);
+			if(i != 2) {
+				txt += ", ";
+			}
+			deck.pop_back();		
+			deck.push_front(deck.back());
+	
+			deck.pop_back();
+		}
+
+		txt += "\nRemaining card: ";
+		txt += (deck.back() + 48);
+		txt += "\n" ;
+		cin >> ent;
+		deck.clear();
 	}
+	cout << txt;
 	return 0;
 }
+
